ef0d14bf7bf111b53700ef302ab476edfc2c49bc
Simon Laurin-Lemay <evol.simon@gmail.com>
2017-08-24 00:04:49 -0400

    readPosterior

--- sources/Posterior.cpp
+++ sources/Posterior.cpp
@@ -532,7 +532,7 @@ void Posterior::readPosterior(ifstream& is)
         string* arr = new string[this->NusedEvoStats];
         while(iss >> w || k < this->NusedEvoStats)
         {
-            w = string("T_") + string(w);
+            w = w.erase(0,2)
             auto it = mapUsedEvoStats.find(w);
             if (it != mapUsedEvoStats.end())
             {
